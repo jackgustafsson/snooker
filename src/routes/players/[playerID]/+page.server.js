538800a351ffcdbfac216f10a9e9c45f13c26ea1
Jack <05guja@skola.engelholm.se>
2024-05-22 16:07:44 +0200

    api

--- src/routes/players/[playerID]/+page.server.js
+++ src/routes/players/[playerID]/+page.server.js
@@ -1,10 +1,11 @@
+import {API_KEY} from '$env/static/private';
 export async function load({ params }) {
     console.log(params.playerID);
     const url = "https://api.snooker.org/?p=" + params.playerID;
 
     const response = await fetch(url, {
         headers: {
-            "X-Requested-By": "JackSkola"
+            "X-Requested-By": API_KEY
         }
     });
     const players = await response.json();
--- src/routes/tours/[tourID]/+page.server.js
+++ src/routes/tours/[tourID]/+page.server.js
@@ -1,10 +1,11 @@
+import {API_KEY} from '$env/static/private';
 export async function load({ params }) {
     console.log(params.tourID);
     const url = "https://api.snooker.org/?e=" + params.tourID;
 
     const response = await fetch(url, {
         headers: {
-            "X-Requested-By": "JackSkola"
+            "X-Requested-By": API_KEY
         }
     });
     const tours = await response.json();
